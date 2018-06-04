47522e7422c6c69d74599cd7c9cf70814e78595b
MikhailTymchukDX <MikhailTymchukDX@users.noreply.github.com>
2018-06-04 18:07:48 +0300

    add missing file

---
+++ CS/LongRunningOperations.Module/BusinessObjects/ObjectForLongRunningOperations.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.Base;
+using DevExpress.Xpo;
+
+namespace LongRunningOperations.Module.BusinessObjects {
+    [DefaultClassOptions]
+    public class ObjectForLongRunningOperations : BaseObject{
+        public ObjectForLongRunningOperations(Session session)
+            : base(session) {
+        }
+        private Object1 obj;
+        public Object1 ObjectToProcess {
+            get { return obj; }
+            set { SetPropertyValue<Object1>("ObejectToProcess", ref obj, value); }
+        }
+    }
+}
